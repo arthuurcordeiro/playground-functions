8edc03a08b88d294f83d2bfb940161cc4e7b8b67
Arthur <acordeiro210691@gmail.com>
2022-10-28 00:06:28 -0300

    Conclui desafio bonus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,35 @@
 // Desafio 11 - Crie uma função de número de telefone
 // Implemente a função generatePhoneNumber que recebe um array com 11 números e retorna um número de telefone, respeitando parênteses, traços e espaços.
-function generatePhoneNumber(telefone) {
-  
+function generatePhoneNumber(telemarketing) {
+  if(telemarketing.length !== 11){
+    return 'Array com tamanho incorreto.'
+  }
+  let repetidos = 0;
+  let numero = '(';
+  for(let i = 0; i < telemarketing.length; i += 1){
+    if(telemarketing[i] < 0 || telemarketing[i] > 9){
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+    for(let j = 0; j < telemarketing.length; j += 1){
+      if(telemarketing[i] === telemarketing[j]){
+        repetidos += 1;
+      }
+    }
+    if(repetidos >= 3){
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+    repetidos = 0;
+    if(i === 2){
+      numero += ') ';
+    }
+    if(i === 7){
+      numero += '-';
+    }
+    numero += telemarketing[i];
+  }
+  return numero;
 }
+
 
 // Desafio 12 - Crie uma função que teste a condição de existência de um triângulo
 // Implemente a função triangleCheck que verifica se é possível formar um triângulo analisando o comprimento de três linhas.
